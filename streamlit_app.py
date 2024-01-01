9cef5602abe6679d3537e4372bdada81623d1bf1
sridharstreaks <sridharstreaks@gmail.com>
2024-01-01 23:17:04 +0530

    Updated to Version 2.0

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import string
 import nltk
-
+import pandas as pd
 
 # Text Transformer
 def transform(string_input):
@@ -41,20 +41,88 @@ def transform(string_input):
 tfidf = pickle.load(open('vectorizer.pkl','rb'))
 model = pickle.load(open('model_bnb.pkl','rb'))
 
-st.title("Spam Detector by Streaks V.1.0")
+def main():
+    # Initialize a session state variable that tracks the sidebar state (either 'expanded' or 'collapsed').
+    if 'sidebar_state' not in st.session_state:
+        st.session_state.sidebar_state = 'collapsed'
 
-input_text=st.text_area("Enter the Message")
+    # Streamlit set_page_config method has a 'initial_sidebar_state' argument that controls sidebar state.
+    st.set_page_config(initial_sidebar_state=st.session_state.sidebar_state)
+    
+    st.title("Spam Detector by Streaks V.2")
 
-if st.button('Predict'):
+    input_text=st.text_area("Enter the Message")
+    input_text=input_text.strip()
 
-    # 1. preprocess
-    transformed_input = transform(input_text)
-    # 2. vectorize
-    vector_input = tfidf.transform([transformed_input])
-    # 3. predict
-    result = model.predict(vector_input)[0]
-    # 4. Display
-    if result == 1:
-        st.header("Spam")
-    else:
-        st.header("Not Spam")
+    if st.button('Predict'):
+
+        if input_text is None or input_text=='':
+            st.error("Please Enter a Text")
+        else:
+            # 1. preprocess
+            transformed_input = transform(input_text)
+            # 2. vectorize
+            vector_input = tfidf.transform([transformed_input])
+            # 3. predict
+            result = model.predict(vector_input)[0]
+            # 4. Display
+            if result == 1:
+                st.header("Spam")
+            else:
+                st.header("Not Spam")
+
+    st.subheader('Not Correct?')
+    st.markdown('Help the model by contributing your data anonymously')
+
+    # Toggle sidebar state between 'expanded' and 'expanded'.
+    if st.button('Contribute!'):
+        st.session_state.sidebar_state = 'expanded' if st.session_state.sidebar_state == 'collapsed' else 'collapsed'
+        # Force an app rerun after switching the sidebar state.
+        st.experimental_rerun()
+    
+    st.sidebar.subheader('Make Contribution Here')
+    contribute_text = st.sidebar.text_area("Enter the Message you want to contribute")
+    contribute_text=contribute_text.strip()
+    contribute_label = st.sidebar.radio("Select the Label", ["0", "1"], index=None,captions=['Not Spam','Spam'])
+
+    if st.sidebar.button('Finish Contribution'):
+        if contribute_text is None or contribute_text=='':
+            st.sidebar.error("Please Enter a Text")
+        else:
+            # Save the contribution to a CSV file
+            contribution_data = {'text': [contribute_text], 'target': [contribute_label]}
+
+            try:
+                contribution_df = pd.read_csv('contributions.csv')
+                contribution_df = pd.concat([contribution_df, pd.DataFrame(contribution_data)], ignore_index=True)
+            except FileNotFoundError:
+                contribution_df = pd.DataFrame(contribution_data)
+                
+            contribution_df.to_csv('contributions.csv', index=False)
+            st.success("Thanks for your contribution!")
+
+    # Read the contributions
+    contribution_df = pd.read_csv('contributions.csv')
+    if len(contribution_df)>=2:
+            try:
+
+                # Transform and vectorize the contributions
+                contribution_df['text'] = contribution_df['text'].apply(transform)
+                X_contribution = tfidf.fit_transformtransform(contribution_df['text'])
+                y_contribution = contribution_df['target']
+
+                # Update the model with new data
+                model.partial_fit(X_contribution, y_contribution, classes=[0, 1])
+                contribution_df.drop(contribution_df.index, inplace=True)
+                contribution_df.to_csv('contributions.csv', index=False)
+
+                st.success("Model Updated")
+
+            except FileNotFoundError:
+                st.error("No contributions found for retraining!")
+
+    
+
+    st.warning('The App still fails to identiy tranctional messages correctly. So, use the app with caution. Also more contributions would be really helpful in training the model on those type of messages', icon="⚠️")
+if __name__=="__main__":
+    main()
